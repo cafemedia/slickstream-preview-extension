c019455f398ec6759c1da6586d919cc9025143f3
Preet Shihn <shihn@Preets-MacBook-Pro.local>
2018-08-24 19:10:32 -0700

    explorer extensions

--- background.js
+++ background.js
@@ -2,19 +2,38 @@ chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.storage.sync.get(['options'], (result) => {
     if (result.options) {
       const options = result.options;
-      const server = options.server || 'https://guild.network/e1/embed.js';
+      const server = options.server || 'https://guild.network/e1/embed-nav.js';
+
+      const css = "`" + (options.css || '').replace('\n', '') + "`";
+
       const code = `
-      if (!window.guild) {
-        window.guild = {
-          site: '${options.siteCode}',
-          tabZIndex: 900
-        };
-        localStorage.setItem('guild-extension-config', JSON.stringify(window.guild));
-        const s = document.createElement('script');
-        s.src = '${server}';
-        document.head.appendChild(s);
-      }
+      ((siteCode, css, selector, scriptUrl) => {
+        if (!window.guild) {
+          window.guild = { site: siteCode };
+          localStorage.setItem('guild-nav-extension-config', JSON.stringify(window.guild));
+          if (css) {
+            const styleNode = document.createElement('style');
+            styleNode.innerHTML = css;
+            document.body.appendChild(styleNode);
+          }
+          if (selector) {
+            const nodes = document.querySelectorAll(selector);
+            if (nodes && nodes.length) {
+              for (let i = 0; i < nodes.length; i++) {
+                const n = nodes[i];
+                const div = document.createElement('div');
+                div.classList.add('guild-explorer');
+                n.insertAdjacentElement('afterend', div);
+              }
+            }
+          }
+          const s = document.createElement('script');
+          s.src = scriptUrl;
+          document.head.appendChild(s);
+        }
+      })('${options.siteCode}', ${css}, '${options.selector}', '${options.server}');
       `;
+      console.log(code);
       chrome.tabs.executeScript({ code });
     }
   });
--- options.js
+++ options.js
@@ -2,20 +2,27 @@
   const btn = document.getElementById('btnSave');
   const siteCode = document.getElementById('siteCode');
   const server = document.getElementById('server');
+  const selector = document.getElementById('selector');
+  const css = document.getElementById('css');
+
   btn.addEventListener('click', () => {
     const options = {
       siteCode: siteCode.value.trim(),
-      server: server.value.trim() || 'https://guild.network/e1/embed.js'
+      server: server.value.trim() || 'https://guild.network/e1/embed-nav.js',
+      selector: selector.value.trim(),
+      css: css.value.trim()
     };
     chrome.storage.sync.set({ options });
   });
   chrome.storage.sync.get(['options'], (result) => {
     if (result.options) {
       const options = result.options;
-      siteCode.value = options.siteCode;
-      server.value = options.server || 'https://guild.network/e1/embed.js'
+      siteCode.value = options.siteCode || '';
+      server.value = options.server || 'https://guild.network/e1/embed-nav.js'
+      selector.value = options.selector || '';
+      css.value = options.css || '';
     } else {
-      server.value = 'https://guild.network/e1/embed.js'
+      server.value = 'https://guild.network/e1/embed-nav.js'
     }
   });
 })();
