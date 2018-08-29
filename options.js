32971132c5d9ac808f7de38243651acdb9cef6b9
Preet Shihn <shihn@Preets-MacBook-Pro.local>
2018-08-29 12:40:44 -0700

    multi site config

--- options.js
+++ options.js
@@ -1,31 +1,97 @@
-(() => {
-  const btn = document.getElementById('btnSave');
-  const siteCode = document.getElementById('siteCode');
-  const server = document.getElementById('server');
-  const selector = document.getElementById('selector');
-  const stripSelector = document.getElementById('stripSelector');
-  const css = document.getElementById('css');
+const btn = document.getElementById('btnSave');
+const btnClear = document.getElementById('btnClear');
+const siteCode = document.getElementById('siteCode');
+const server = document.getElementById('server');
+const selector = document.getElementById('selector');
+const stripSelector = document.getElementById('stripSelector');
+const css = document.getElementById('css');
+const host = document.getElementById('host');
+const sitesPanel = document.getElementById('sitesPanel');
 
-  btn.addEventListener('click', () => {
-    const options = {
-      siteCode: siteCode.value.trim(),
-      server: server.value.trim() || 'https://guild.network/e1/embed-nav.js',
-      selector: selector.value.trim(),
-      stripSelector: stripSelector.value.trim(),
-      css: css.value.trim()
-    };
-    chrome.storage.sync.set({ options });
-  });
-  chrome.storage.sync.get(['options'], (result) => {
-    if (result.options) {
-      const options = result.options;
-      siteCode.value = options.siteCode || '';
-      server.value = options.server || 'https://guild.network/e1/embed-nav.js'
-      selector.value = options.selector || '';
-      stripSelector.value = options.stripSelector || '';
-      css.value = options.css || '';
-    } else {
-      server.value = 'https://guild.network/e1/embed-nav.js'
+let serverUrl = '';
+let data = {};
+let currentHost = '';
+
+function onCardSave() {
+  let hostUrl = host.value.trim();
+  if (hostUrl) {
+    try {
+      hostUrl = new URL(hostUrl).host;
+    } catch (err) {
     }
+  }
+  const options = {
+    host: hostUrl,
+    siteCode: siteCode.value.trim(),
+    selector: selector.value.trim(),
+    stripSelector: stripSelector.value.trim(),
+    css: css.value.trim()
+  };
+  currentHost = hostUrl;
+  if (currentHost) {
+    data[currentHost] = options;
+  }
+  doSave();
+}
+
+function doSave() {
+  const toSave = {
+    siteData: data,
+    server: server.value.trim() || 'https://guild.network/e1/embed-nav.js'
+  }
+  chrome.storage.sync.set(toSave);
+  refreshSites();
+}
+
+async function load() {
+  chrome.storage.sync.get(['siteData', 'server'], (result) => {
+    data = result.siteData || {};
+    serverUrl = result.server || 'https://guild.network/e1/embed-nav.js';
+    server.value = serverUrl;
+    refreshSites();
   });
-})();
+}
+
+function clearCard() {
+  siteCode.value = '';
+  selector.value = '';
+  stripSelector.value = '';
+  css.value = '';
+  host.value = '';
+}
+
+function refreshSites() {
+  while (sitesPanel.hasChildNodes() && sitesPanel.lastChild) {
+    sitesPanel.removeChild(sitesPanel.lastChild);
+  }
+  for (const hostName in data) {
+    if (hostName) {
+      const div = document.createElement('div');
+      div.classList.add('hostCard');
+      div.textContent = data[hostName].host;
+      const x = document.createElement('button');
+      x.textContent = 'delete';
+      div.appendChild(x);
+      x.addEventListener('click', (e) => {
+        e.stopPropagation();
+        delete data[hostName];
+        clearCard();
+        doSave();
+      });
+      div.addEventListener('click', () => {
+        const d = data[hostName];
+        siteCode.value = d.siteCode;
+        selector.value = d.selector;
+        stripSelector.value = d.stripSelector;
+        css.value = d.css;
+        host.value = d.host;
+      });
+      sitesPanel.appendChild(div);
+    }
+  }
+}
+
+btn.addEventListener('click', () => onCardSave());
+btnClear.addEventListener('click', () => clearCard());
+
+load();
