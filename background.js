c5529b849c3a28c87f0472a0f8631ce71f65539d
Preet Shihn <shihn@Preets-MacBook-Pro.local>
2018-08-28 17:03:43 -0700

    embedding of film strip

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ chrome.browserAction.onClicked.addListener(function (tab) {
       const css = "`" + (options.css || '').replace('\n', '') + "`";
 
       const code = `
-      ((siteCode, css, selector, scriptUrl) => {
+      ((siteCode, css, selector, stripSelector, scriptUrl) => {
         if (!window.guild) {
           window.guild = { site: siteCode };
           localStorage.setItem('guild-nav-extension-config', JSON.stringify(window.guild));
@@ -27,11 +27,22 @@ chrome.browserAction.onClicked.addListener(function (tab) {
               }
             }
           }
+          if (stripSelector) {
+            const nodes = document.querySelectorAll(stripSelector);
+            if (nodes && nodes.length) {
+              for (let i = 0; i < nodes.length; i++) {
+                const n = nodes[i];
+                const div = document.createElement('div');
+                div.classList.add('guild-film-strip');
+                n.insertAdjacentElement('afterend', div);
+              }
+            }
+          }
           const s = document.createElement('script');
           s.src = scriptUrl;
           document.head.appendChild(s);
         }
-      })('${options.siteCode}', ${css}, '${options.selector}', '${options.server}');
+      })('${options.siteCode}', ${css}, '${options.selector}', '${options.stripSelector}', '${options.server}');
       `;
       console.log(code);
       chrome.tabs.executeScript({ code });
--- options.js
+++ options.js
@@ -3,6 +3,7 @@
   const siteCode = document.getElementById('siteCode');
   const server = document.getElementById('server');
   const selector = document.getElementById('selector');
+  const stripSelector = document.getElementById('stripSelector');
   const css = document.getElementById('css');
 
   btn.addEventListener('click', () => {
@@ -10,6 +11,7 @@
       siteCode: siteCode.value.trim(),
       server: server.value.trim() || 'https://guild.network/e1/embed-nav.js',
       selector: selector.value.trim(),
+      stripSelector: stripSelector.value.trim(),
       css: css.value.trim()
     };
     chrome.storage.sync.set({ options });
@@ -20,6 +22,7 @@
       siteCode.value = options.siteCode || '';
       server.value = options.server || 'https://guild.network/e1/embed-nav.js'
       selector.value = options.selector || '';
+      stripSelector.value = options.stripSelector || '';
       css.value = options.css || '';
     } else {
       server.value = 'https://guild.network/e1/embed-nav.js'
