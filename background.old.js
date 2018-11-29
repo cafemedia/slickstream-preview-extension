57537031acdf5260093a4fde4529339208bc1b11
Preet Shihn <preetshihn@gmail.com>
2018-11-28 16:14:01 -0800

    new stuff WIP

---
+++ background.old.js
@@ -0,0 +1,67 @@
+let serverUrl = '';
+
+async function getSiteData() {
+  return new Promise((resolve) => {
+    chrome.storage.sync.get(['siteData', 'server'], (result) => {
+      const siteData = result.siteData;
+      serverUrl = result.server || 'https://poweredbyslick.com//e1/embed-nav.js';
+      resolve(siteData || {});
+    });
+  });
+}
+
+function inject(options) {
+  let scriptUrl = serverUrl;
+  if (options.siteCode) {
+    scriptUrl += '?site=' + encodeURIComponent(options.siteCode);
+  }
+  const css = "`" + (options.css || '').replace('\n', '') + "`";
+  const code = `
+    ((siteCode, css, selector, stripSelector, scriptUrl, stripPosition, explorerPosition, filmStripToolbar, linkHighlighter) => {
+      stripPosition = stripPosition || 'after selector';
+      explorerPosition = explorerPosition || 'after selector';
+      if (!window.slick) {
+        window.slick = { site: siteCode };
+        if (selector) {
+          window.slick.explorer = {
+            position: explorerPosition,
+            selector: selector
+          };
+        }
+        if (stripSelector) {
+          window.slick.filmStrip = {
+            position: stripPosition,
+            selector: stripSelector
+          };
+        }
+        if (filmStripToolbar) {
+          window.slick.filmStripToolbar = { state: 'enabled' };
+        }
+        if (linkHighlighter) {
+          window.slick.linkHighlighter = { state: 'enabled' };
+        }
+        localStorage.setItem('slick-nav-extension-config-2', JSON.stringify(window.slick));
+        if (css) {
+          const styleNode = document.createElement('style');
+          styleNode.innerHTML = css;
+          document.body.appendChild(styleNode);
+        }
+        const s = document.createElement('script');
+        s.src = scriptUrl;
+        document.head.appendChild(s);
+      }
+    })('${options.siteCode}', ${css}, '${options.selector}', '${options.stripSelector}', '${scriptUrl}', '${options.stripPosition}', '${options.explorerPosition}', ${options.filmStripToolbar}, ${options.linkHighlighter});
+  `;
+  console.log(code);
+  chrome.tabs.executeScript({ code });
+}
+
+chrome.webNavigation.onDOMContentLoaded.addListener(async (data) => {
+  if (!data.frameId) {
+    const host = new URL(data.url).host;
+    const siteData = await getSiteData();
+    if (siteData[host] && siteData[host].siteCode) {
+      inject(siteData[host])
+    }
+  }
+});
